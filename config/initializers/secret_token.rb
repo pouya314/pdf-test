9c7a3be36eb30135c057213305005733219e8973
Pouya Arvandian <pouya314@gmail.com>
2013-12-13 16:56:00 +0800

    made pdf generation work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PdfTest::Application.config.secret_key_base = '7d63ae5da2c1f1541cc0b553915e2e96a6ddedf30785a412a22a287fd867bf9d44a60a0737ae67108f5f7445a835d224be2bbc5204a5f9e99f4c01c6c3f9f109'
