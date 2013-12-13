05ebd12874364d2e67c43f4c91221fd36ebe8439
Pouya Arvandian <pouya314@gmail.com>
2013-12-13 17:06:50 +0800

    fixing the wkhtmltopdf executable file path for heroku deployment

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,6 +1,7 @@
 # config/initializers/pdfkit.rb
 PDFKit.configure do |config|
-  config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
+  # config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
+  config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
   # config.default_options = {
   #   :page_size => 'Legal',
   #   :print_media_type => true
